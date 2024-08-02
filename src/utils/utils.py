9fb76305a0fc664e28ac5a53e7dabd654b270ba5
snehsuresh <snehsuresh002@gmail.com>
2024-08-02 16:23:06 -0500

    evaluation added

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,12 +1,10 @@
 import os
 import sys
 import pickle
-import numpy as np
-import pandas as pd
 from src.logger.loging import logging
 from src.exception.exception import customexception
 import cv2 as cv
-from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.metrics import r2_score
 from tensorflow.keras.models import load_model
 
 
